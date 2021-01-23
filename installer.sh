769845443f334c0d21fa8831ec1d4e137e999fe2
Alexandr-telegram <77691414+Alexandr-telegram@users.noreply.github.com>
2021-01-23 18:22:57 +0300

    v 3
    work

--- installer.sh
+++ installer.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 clear
 echo "------------------------------------------------------------"
 echo "|     Что ты хочешь установить?                            |"
 echo "|----------------------------------------------------------|"
 echo "| 1. SMS-Бомберы                                           |"
-echo "| 2. DOS-еры                                               |"
-echo "| 3. Всё что связано с фишингом                            |"
+echo "| 2. Всё что связано с фишингом                            |"
+echo "| 3. DOS-еры                                               |"
 echo "|                                                          |"
 echo "| Введите 1/2/3:                                           |"
 echo "------------------------------------------------------------"
 read numb
 if [ $numb = "1" ]
 then
-	cd ..
+	pkg install python
 	pkg update
 	pkg upgrade
 	apt update
@@ -20,40 +21,18 @@ then
 	pkg install php
 	pkg install python2
 	pkg install git
-	mkdir sms-spammers
-	cd sms-spammers
-	git clone https://github.com/Ivan-Hacker-700/SMS-Bomber-300-Free
-	git clone https://github.com/FSystem88/spymer
-	git clone https://github.com/LimerBoy/Impulse
-	git clone https://github.com/pentestxr/Fludilka
-	git clone https://github.com/Denishnc/b0mb3r
-	git clone https://github.com/nandydark/Russianbomber
-
-if [ $numb = "2" ]
-then
-	cd ..
-	mkdir ddos
-	cd ddos
-	pkg update
-	pkg upgrade
-	apt update
-	apt upgrade
-	pkg install python
-	pkg install php
-	pkg install python2
-	pkg install git
-	git clone https://github.com/649/Memcrashed-DDoS-Exploit/
-	git clone https://github.com/LimerBoy/Impulse
-	git clone https://github.com/RioBithub/ddos-termux
-	git clone https://github.com/cyweb/hammer
-	git clone https://github.com/wenfengshi/ddos-dos-tools
-		fi
-	else
-		if [ $numb = "3" ] 
-		then
+	git clone https://github.com/samyoyo/weeman
+	git clone https://github.com/thewhiteh4t/seeker
+	git clone https://github.com/UndeadSec/SocialFish
+	git clone https://github.com/mrleedev/say-cheese
+	git clone https://github.com/greyli/sayhello
+else
+	if [ $numb = "2" ]
+	then
+		else
 			cd ..
-			mkdir fishing
-			cd fishing
+			mkdir ddos
+			cd ddos
 			pkg update
 			pkg upgrade
 			apt update
@@ -62,11 +41,32 @@ then
 			pkg install php
 			pkg install python2
 			pkg install git
-			git clone https://github.com/samyoyo/weeman
-			git clone https://github.com/thewhiteh4t/seeker
-			git clone https://github.com/UndeadSec/SocialFish
-			git clone https://github.com/mrleedev/say-cheese
-			git clone https://github.com/greyli/sayhello
+			git clone https://github.com/649/Memcrashed-DDoS-Exploit/
+			git clone https://github.com/LimerBoy/Impulse
+			git clone https://github.com/RioBithub/ddos-termux
+			git clone https://github.com/cyweb/hammer
+			git clone https://github.com/wenfengshi/ddos-dos-tools
+		fi
+	else
+		if [ $numb = "3" ] 
+		then
+			cd ..
+			mkdir ddos
+			cd ddos
+			pkg update
+			pkg upgrade
+			apt update
+			apt upgrade
+			pkg install python
+			pkg install php
+			pkg install python2
+			pkg install git
+			git clone https://github.com/649/Memcrashed-DDoS-Exploit/
+			git clone https://github.com/LimerBoy/Impulse
+			git clone https://github.com/RioBithub/ddos-termux
+			git clone https://github.com/cyweb/hammer
+			git clone https://github.com/wenfengshi/ddos-dos-tools
+
 		else
 			echo "Некорректный ввод"
 		fi
